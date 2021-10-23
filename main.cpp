c95e0508f6be3c5766a3f9aa4960124ea6e134c0
T0nd0Tara <5632815@gmail.com>
2021-10-23 13:15:05 +0300

    remove still jaggedy bug

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #define OLC_PGE_APPLICATION
 #include "olcPixelGameEngine.h"
-#define TARA_PGE_EXTENSION
-#include <taraNS.h>
+
 #include <iostream>
 
 #include "Segment.h"
@@ -15,34 +14,40 @@ public:
 
 private:
 	std::vector<Segment*> vec;
-	uint8_t segmentNum = 8;
+	uint8_t segmentNum = 15;
+	olc::vi2d LastMousePos = { 0,0 };
 
 protected:
 	bool OnUserCreate() override {
 		vec.push_back(new Segment({ ScreenWidth() >> 1, ScreenHeight() >> 1 }, 20, 0));
 		for (uint8_t i = 1; i < segmentNum; i++)
-			vec.push_back(new Segment(vec.back(), 20, 0));
+			vec.push_back(new Segment(vec.back(), 12, 0));
 
 		return true;
 	}
 
 	bool OnUserUpdate(float elapsedTime) override {
-		Clear(olc::BLACK);
 		if (GetKey(olc::ESCAPE).bPressed) return false;
-
-		vec.back()->Update(GetMousePos());
+		if (LastMousePos == GetMousePos()) return true;
+		LastMousePos = GetMousePos();
+		Clear(olc::BLACK);
+		
+		vec.back()->Update(LastMousePos);
 		vec.back()->MoveA(vec[0]->a - olc::vi2d{ ScreenWidth() >> 1, ScreenHeight() >> 1 });
+		
 		vec.back()->Draw(this);
 
-		Sleep(20);
+		Sleep(30);
+		
 		//std::cout << a->angle << '\n';
 		return true;
 	}
 };
 
 int main() {
+	uint8_t res = 1;
 	InverseKinematics demo;
-	if (demo.Construct(512, 480, 2, 2))
+	if (demo.Construct(256 << res, 240 << res, 4 >> res, 4 >> res))
 		demo.Start();
 	return 0;
 }
